4e9d1e52e5a665218942d594ac1262f68036ec7f
Fabian Lam <gfabian.lam@gmail.com>
2020-03-23 21:43:32 -0600

    Initial commit.

---
+++ Babel.Service/Entities/BookDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Babel.Service.Entities
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string NoVolumen { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Babel.Service/Interfaces/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Babel.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel.Service.Interfaces
+{
+    public interface IBookService
+    {
+        Task<IList<BookDto>> GetAll();
+        Task<BookDto> GetFirst();
+        Task<BookDto> GetLast();
+        Task Create(BookDto newBook);
+        Task Delete(int id);
+    }
+}
